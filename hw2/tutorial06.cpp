a397c11b6e76447779e85f33142025fdf7c14119
AnisimovaVictoria <anisimova.vs@phystech.edu>
2020-04-17 21:11:03 +0300

    Add files via upload

--- hw2/tutorial06.cpp
+++ hw2/tutorial06.cpp
@@ -1,6 +1,9 @@
 // Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
 
 // Include GLEW
 #include <GL/glew.h>
@@ -17,6 +20,8 @@ using namespace glm;
 #include <common/shader.hpp>
 #include <common/texture.hpp>
 #include <common/controls.hpp>
+
+const int ENEMY_NUMBER = 10;
 
 int main( void )
 {
@@ -66,31 +71,29 @@ int main( void )
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
 	// Enable depth test
-	//glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 	// Accept fragment if it closer to the camera than the former one
-	//glDepthFunc(GL_LESS); 
+	glDepthFunc(GL_LESS); 
 
 	// Cull triangles which normal is not towards the camera
-	//glEnable(GL_CULL_FACE);
+	glEnable(GL_CULL_FACE);
 
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-  GLuint floorID = LoadShaders("TransformVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
-	//GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader" );
+  GLuint FloorID = LoadShaders("TransformVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+	GLuint FloorMatrixID = glGetUniformLocation(FloorID, "MVP");
+  
+	GLuint EnemyID = LoadShaders("EnemyTransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader");
+	GLuint EnemyMatrixID = glGetUniformLocation(EnemyID, "MVP");
 	
-
-	// Get a handle for our "MVP" uniform
-	//GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-	GLuint FloorMatrixID = glGetUniformLocation(floorID, "MVP");
-
 	// Load the texture
 	GLuint Texture = loadDDS("uvtemplate.DDS");
 	
 	// Get a handle for our "myTextureSampler" uniform
-	GLuint TextureID  = glGetUniformLocation(floorID, "myTextureSampler");
+	GLuint TextureID  = glGetUniformLocation(FloorID, "myTextureSampler");
 
 	//floor
 	static const GLfloat g_vertex_buffer_data_floor[] = {
@@ -101,18 +104,107 @@ int main( void )
 		 100.0f, 0.0f, 100.0f,
 		 100.0f, 0.0f,-100.0f,
 		-100.0f, 0.0f,-100.0f,
-	};
-
+	};	
 
 	GLuint floorvertexbuffer;
 	glGenBuffers(1, &floorvertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, floorvertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data_floor), g_vertex_buffer_data_floor, GL_STATIC_DRAW);
 
+	static const GLfloat g_vertex_buffer_data_enemy[] = {
+	 0.0f, 1.2f, 0.0f,
+	-1.0f, 0.0f, 1.0f,
+	 1.0f, 0.0f, 1.0f,
+
+	 0.0f, 1.2f, 0.0f,
+	 1.0f, 0.0f, 1.0f,
+	 1.0f, 0.0f,-1.0f,
+
+	 0.0f, 1.2f, 0.0f,
+	 1.0f, 0.0f,-1.0f,
+	-1.0f, 0.0f,-1.0f,
+
+	 0.0f, 1.2f, 0.0f,
+	-1.0f, 0.0f,-1.0f,
+	-1.0f, 0.0f, 1.0f,
+
+	 0.0f,-1.2f, 0.0f,
+	 1.0f, 0.0f, 1.0f,
+	-1.0f, 0.0f, 1.0f,
+
+	 0.0f,-1.2f, 0.0f,
+	 1.0f, 0.0f,-1.0f,
+	 1.0f, 0.0f, 1.0f,
+
+	 0.0f,-1.2f, 0.0f,
+	-1.0f, 0.0f,-1.0f,
+	 1.0f, 0.0f,-1.0f,
+
+	 0.0f,-1.2f, 0.0f,
+	-1.0f, 0.0f, 1.0f,
+	-1.0f, 0.0f,-1.0f,
+
+	};
+
+	// One color for each vertex. They were generated randomly.
+	static const GLfloat g_color_buffer_data_enemy[] = {
+	0.583f,  0.771f,  0.9f,
+	0.609f,  0.115f,  0.9f,
+	0.327f,  0.483f,  0.844f,
+
+	0.822f,  0.569f,  0.9f,
+	0.435f,  0.602f,  0.9f,
+	0.310f,  0.747f,  0.9f,
+
+	0.597f,  0.770f,  0.761f,
+	0.559f,  0.436f,  0.730f,
+	0.359f,  0.583f,  0.9f,
+
+	0.483f,  0.596f,  0.789f,
+	0.559f,  0.861f,  0.639f,
+	0.195f,  0.548f,  0.859f,
+
+	0.014f,  0.184f,  0.576f,
+	0.771f,  0.328f,  0.970f,
+	0.406f,  0.615f,  0.9f,
+
+	0.676f,  0.977f,  0.9f,
+	0.971f,  0.572f,  0.833f,
+	0.140f,  0.616f,  0.489f,
+
+	0.997f,  0.513f,  0.9f,
+	0.945f,  0.719f,  0.9f,
+	0.543f,  0.021f,  0.978f,
+
+	0.279f,  0.317f,  0.9f,
+	0.167f,  0.620f,  0.9f,
+	0.347f,  0.857f,  0.8f,
+
+	};
+
+	GLuint enemyvertexbuffer;
+	glGenBuffers(1, &enemyvertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, enemyvertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data_enemy), g_vertex_buffer_data_enemy, GL_STATIC_DRAW);
+
+	GLuint enemycolorbuffer;
+	glGenBuffers(1, &enemycolorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, enemycolorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data_enemy), g_color_buffer_data_enemy, GL_STATIC_DRAW);
+
+	float enemy_coords[ENEMY_NUMBER][3];
+	float enemy_radius = 10.0f;
+	for (int i = 0; i < ENEMY_NUMBER; i++) {
+		enemy_coords[i][0] = (float)std::rand() / RAND_MAX * 2 * enemy_radius - enemy_radius;
+		enemy_coords[i][1] = (float)std::rand() / RAND_MAX * enemy_radius;
+		enemy_coords[i][2] = (float)std::rand() / RAND_MAX * 2 * enemy_radius - enemy_radius;
+		std::cout << enemy_coords[i][0] << " ";
+	}
+	float lastTime = glfwGetTime();
 	do{
 
 		// Clear the screen
-		glClear(GL_COLOR_BUFFER_BIT); //| GL_DEPTH_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Compute the MVP matrix from keyboard and mouse input
 		computeMatricesFromInputs();
@@ -120,19 +212,14 @@ int main( void )
 		glm::mat4 ViewMatrix = getViewMatrix();
 		glm::mat4 ModelMatrix = glm::mat4(1.0);
 		glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
-		// Use our shader
-		glUseProgram(floorID);
 		
-
+		//FLOOR
+		// Use our shader
+		glUseProgram(FloorID);
+		
 		// Send our transformation to the currently bound shader, 
 		// in the "MVP" uniform
 		glUniformMatrix4fv(FloorMatrixID, 1, GL_FALSE, &MVP[0][0]);
-
-		// Bind our texture in Texture Unit 0
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, Texture);
-		// Set our "myTextureSampler" sampler to use Texture Unit 0
-		glUniform1i(TextureID, 0);
 
 		// 1rst attribute buffer : vertices
 		glEnableVertexAttribArray(0);
@@ -146,30 +233,57 @@ int main( void )
 			(void*)0            // array buffer offset
 		);
 
-		// 2nd attribute buffer : UVs
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(
-			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-			2,                                // size : U+V => 2
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
-		);
-
 		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
-		// Send our transformation to the currently bound shader, 
-		// in the "MVP" uniform
-		glUniformMatrix4fv(floorID, 1, GL_FALSE, &MVP[0][0]);
+		glDrawArrays(GL_TRIANGLES, 0, 2*3); // 12*3 indices starting at 0 -> 12 triangles
+		
+		//ENEMY
+		glUseProgram(EnemyID);
 
-		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // 12*3 indices starting at 0 -> 12 triangles
+		for (int i = 0; i < ENEMY_NUMBER; i++) {
+		  // Enemies appear eventually
+			if (glfwGetTime() - lastTime < i)
+				continue;
+
+			ModelMatrix = glm::mat4(1.0);
+			ModelMatrix[3][0] = enemy_coords[i][0];
+			ModelMatrix[3][1] = enemy_coords[i][1];
+			ModelMatrix[3][2] = enemy_coords[i][2];
+
+			MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+			// Send our transformation to the currently bound shader, 
+			// in the "MVP" uniform
+			glUniformMatrix4fv(EnemyMatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+			// 1rst attribute buffer : vertices
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, enemyvertexbuffer);
+			glVertexAttribPointer(
+				0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+				3,                  // size
+				GL_FLOAT,           // type
+				GL_FALSE,           // normalized?
+				0,                  // stride
+				(void*)0            // array buffer offset
+			);
+
+			// 2nd attribute buffer : colors
+			glEnableVertexAttribArray(1);
+			glBindBuffer(GL_ARRAY_BUFFER, enemycolorbuffer);
+			glVertexAttribPointer(
+				1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+				3,                                // size
+				GL_FLOAT,                         // type
+				GL_FALSE,                         // normalized?
+				0,                                // stride
+				(void*)0                          // array buffer offset
+			);
+
+			// Draw the triangle !
+			glDrawArrays(GL_TRIANGLES, 0, 8 * 3);
+		}
 
 		glDisableVertexAttribArray(0);
-		//glDisableVertexAttribArray(1);
-
-		// Swap buffers
+		glDisableVertexAttribArray(1);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
@@ -182,7 +296,7 @@ int main( void )
 	
 	//glDeleteBuffers(1, &vertexbuffer);
 	//glDeleteBuffers(1, &uvbuffer);
-	glDeleteProgram(floorID);
+	glDeleteProgram(FloorID);
 	//glDeleteProgram(programID);
 	//glDeleteTextures(1, &TextureID);
 	glDeleteVertexArrays(1, &VertexArrayID);
